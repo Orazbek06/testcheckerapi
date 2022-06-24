280236a2499957cdcad253c9608b2f8124e14af5
Orazbek Tulaganov <orazbektulaganov2002ernaraydar@gmail.com>
2022-06-24 15:47:25 +0500

    added org komp

--- src/main/java/com/orazbek_tulaganov/test/checker/Controller.java
+++ src/main/java/com/orazbek_tulaganov/test/checker/Controller.java
@@ -9,6 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,14 +23,15 @@ public class Controller {
     Set<Integer> got;
     public static final Random random = new Random();
 
-    @GetMapping("/")
-    public Tests main() throws IOException {
+    @GetMapping("/{name}")
+    public Tests main(@PathVariable String name) throws IOException {
         Tests tests1 = new Tests();
         TestList = new ArrayList<>();
         got = new HashSet<>();
         InputStream inputStream;
-        ClassPathResource resource = new ClassPathResource("/test.xlsx");
-
+        ClassPathResource resource;
+        if(name.equals("ok"))resource = new ClassPathResource("/okresult.xlsx");
+        else resource = new ClassPathResource("/test.xlsx");
         try {
             inputStream = resource.getInputStream();
         } catch (FileNotFoundException e) {
@@ -59,19 +61,61 @@ public class Controller {
             javoblar.add("");
             javoblar.add("");
             javoblar.add("");
+            CellType cellType = cell.getCellType();
+            switch (cellType){
+                case NUMERIC:
+                    test.setQuestion(String.valueOf(cell.getNumericCellValue()));
+                    break;
+                case STRING:
+                    test.setQuestion(cell.getStringCellValue());
+                    break;
+                case BOOLEAN:
+                    test.setQuestion(String.valueOf(cell.getBooleanCellValue()));
+                    break;
+                case _NONE:
+                    test.setQuestion("");
+                    break;
+            }
 
-            test.setQuestion(String.valueOf(cell.getNumericCellValue()));
+
 
             cell = cellIterator.next();
 
             test.setRight(String.valueOf((char) ('A' + rightVariant)));
 
-            javoblar.set(rightVariant, cell.getStringCellValue());
+            cellType = cell.getCellType();
+            switch (cellType){
+                case NUMERIC:
+                    javoblar.set(rightVariant, String.valueOf(cell.getNumericCellValue()));
+                    break;
+                case STRING:
+                    javoblar.set(rightVariant, cell.getStringCellValue());
+                    break;
+                case BOOLEAN:
+                    javoblar.set(rightVariant,String.valueOf(cell.getBooleanCellValue()));
+                    break;
+                case _NONE:
+                    javoblar.set(rightVariant,"");
+                    break;
+            }
 
             for (int j = 0; j < javoblar.size(); j++) {
                 if (j != rightVariant) {
-                    cell = cellIterator.next();
-                    javoblar.set(j, cell.getStringCellValue());
+                    cell = cellIterator.next();cellType = cell.getCellType();
+                    switch (cellType){
+                        case NUMERIC:
+                            javoblar.set(j, String.valueOf(cell.getNumericCellValue()));
+                            break;
+                        case STRING:
+                            javoblar.set(j, cell.getStringCellValue());
+                            break;
+                        case BOOLEAN:
+                            javoblar.set(j,String.valueOf(cell.getBooleanCellValue()));
+                            break;
+                        case _NONE:
+                            javoblar.set(j,"");
+                            break;
+                    }
                 }
             }
             test.setA(javoblar.get(0));
